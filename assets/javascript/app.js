34ec3ce26413bd997f9a0f9a2a5c8b20d8ca9064
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-10-17 13:06:42 -0700

    Added timer function, need to give it a way to dynamically write to HTML. Then need questions, scoring system, and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,27 @@ var unanswered = 0;
 
 // Timer 
 
+var count = 2;
+
+function timer() {
+    count = count - 1;
+    if (count <= 0) {
+        gameOver();
+    }
+}
+
+// Game Over Function
+function gameOver() {
+    $(".cont-start").hide();
+    $(".cont-game").hide();
+    $(".cont-over").show();
+}
+
 
 
 $(document).ready(function() {
 
+    // Displays score screen for gameOver function
     $("#right-answers").html("Right Answers: " + answerRight);
     $("#wrong-answers").html("Wrong Answers: " + answerWrong);
     $("#unanswered").html("Unanswered: " + unanswered);
@@ -18,9 +35,13 @@ $(document).ready(function() {
     $(".cont-over").hide();
 
     $("#start").click(function() {
-        $(".cont-start").hide(); // Modify this once timer is in place
-        $(".cont-game").show(); // Modify this once timer is in place
-        $(".cont-over").show(); // Modify this once timer is in place
+
+        // Timer
+        var counter = setInterval(timer, 1000);
+
+        $(".cont-start").hide();
+        $(".cont-game").show();
+        $(".cont-over").hide();
 
         
     });
