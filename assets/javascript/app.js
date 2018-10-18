956037a0ac24ff099b2c213423df5d8561ff7cd6
Gabriel Beita-Kiser <gbeitakiser@gmail.com>
2018-10-17 21:05:44 -0700

    right/wrong answer logic in place. Need to print it to game over screen then find a way to only check one box at a time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,10 +2,7 @@
 
 var answerRight = 0;
 var answerWrong = 0;
-var unanswered = 0;
-console.log(answerRight);
-console.log(answerWrong);
-console.log(unanswered);
+var unanswered = 10;
 
 // Timer 
 
@@ -55,7 +52,37 @@ $(document).ready(function() {
         //     $('input[type="checkbox"]').not(this).prop('checked', false);      
         // });
 
-        // Listens for checked boxes and adds response to Questions Right or Questions Wrong depending on whether it's correct
+        // 
+        // Listens for checked boxes and adds response to Questions Right or Questions Wrong depending on which checkbox is clicked
+        var checkboxesF = $('input[name="false"]');
+        checkboxesF.change(function() {
+            answerWrong++;
+            unanswered--;
+            console.log("False " + this.checked);
+            console.log("Wrong Answers: " + answerWrong);
+            console.log("Unanswered: " + unanswered);
+        });
+
+        var checkboxesT = $('input[name="true"]');
+        checkboxesT.change(function() {
+            answerRight++;
+            unanswered--;
+            console.log("True " + this.checked);
+            console.log("Right Answers: " + answerRight);
+            console.log("Unanswered: " + unanswered);
+        });
+
+        // Code Here
+
+
+        
+
+
+        // Code Here
+
+
+
+        // 
            
 
         // Displays score screen for gameOver function
